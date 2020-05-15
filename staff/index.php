317b59b77b86ded6b055832ee59631341f78df5f
Jamie Phelps <james.phelps1995@live.com>
2020-05-15 12:39:36 +0100

    removed spacing

--- staff/index.php
+++ staff/index.php
@@ -3,14 +3,14 @@
 <?php $title = 'Home'; ?>
 <?php include('../private/shared/header.php'); ?>
 
-    <div id="content">
-      <div id="main-menu">
-        <h2>Main Menu</h2>
-        <ul>
-          <li><a href="<?php echo url_for('/staff/bookings/index.php'); ?>">Bookings</a></li>
-        </ul>
-      </div>
+<div id="content">
+  <div id="main-menu">
+    <h2>Main Menu</h2>
+    <ul>
+      <li><a href="<?php echo url_for('/staff/bookings/index.php'); ?>">Bookings</a></li>
+    </ul>
+  </div>
 
-    </div>
+</div>
 
 <?php include('../private/shared/footer.php'); ?>
