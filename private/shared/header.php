68f18c41db433c519c03eb540e4a3e181af2a4ef
Jamie Phelps <james.phelps1995@live.com>
2020-05-15 12:09:13 +0100

    added staff / private areas

---
+++ private/shared/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Bookings - <?php echo $title ?></title>
+    <link rel="stylesheet" href="../css/styles_php.css">
+  </head>
+  <body>
+    <header>
+      <h1>Dine Restaurant - Bookings</h1>
+    </header>
+
+    <navigation>
+      <ul>
+        <li><a href="<?php echo ('../staff/index.php'); ?>">Menu</a></li>
+      </ul>
+    </navigation>
---
+++ staff/index.php
@@ -0,0 +1,16 @@
+<?php $title = 'Home'?>
+<?php include('../private/shared/header.php') ?>
+
+    <div id="content">
+      <div id="main-menu">
+        <h2>Main Menu</h2>
+        <ul>
+          <li><a href="<?php echo ('bookings/index.php'); ?>">Bookings</a></li>
+        </ul>
+      </div>
+
+    </div>
+
+    <footer><?php echo date('Y'); ?></footer>
+  </body>
+</html>
