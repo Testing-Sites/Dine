a93e1513afc59c3df42480f42e23b2098a9ba14e
Jamie Phelps <james.phelps1995@live.com>
2020-05-19 13:44:36 +0100

    added form into main site

---
+++ form.php
@@ -0,0 +1,15 @@
+<?php require_once('private/initialize.php'); ?>
+
+<?php $title = 'Booking'; ?>
+<?php include(SHARED_PATH . '/booking_header.php'); ?>
+
+  <header>
+    <div class="background">
+      <img  class="logo" src="img/logo.png" alt="logo for dine, font lowercase, bold, white">
+      <h1>Reservations</h1>
+      <p>We can’t wait to host you. If you have any special requirements please feel free to call on the phone number below. We’ll be happy to accommodate you.</p>
+    </div>
+  </header>
+
+  </body>
+</html>
---
+++ private/shared/booking_header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" href="css/styles_form.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dine <?php echo $title ?></title>
+  </head>
+  <body>
